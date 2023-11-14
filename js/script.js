886032069bb2d15532ab7e1a3b17d36a8381ee6b
Alejandro López Fernández <alex14lf@gmail.com>
2023-11-14 19:27:36 +0100

    add the array of words

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let palabras = [
+    "mesas", "actor", "calle", "hojas", "avion", "fuego", "calas", "balas", "luces", "calma",
+    "panes", "besos", "nubes", "cielo", "rojas", "niños", "valor", "risas", "reloj", "feliz",
+    "lagos", "mosca", "dulce", "silla", "verde", "camas", "casas", "leche", "marco", "furia",
+    "playa", "sabor", "noche", "celda", "cajas", "libro", "bueno", "fruta", "tarta", "canto",
+    "perla", "torre", "luzca", "truco", "vuelo", "balon", "brote", "chico", "patio", "tejas",
+    "focas", "gafas", "dedos", "curva", "curvo", "papel", "vista", "tinta", "pleno", "perro",
+    "selva", "pesca", "finca", "vuela", "finas", "grasa", "horno", "aviso", "juego", "cuero",
+    "toros", "marca", "tanto", "verso", "parte", "bajos", "balsa", "bolsa", "traer", "zorro",
+    "cuota", "broma", "recio", "crudo", "suelo", "sordo", "naves", "flora", "vozca", "chile",
+    "resta", "tozco", "sutil", "melon", "campo", "rizos", "cesto", "monte", "hielo", "sabio",
+    "corro", "islas", "cerdo", "raton", "tonto", "pulso", "heroe", "genio", "tenis", "pilar",
+    "atomo", "gente", "poder", "lugar", "clara", "feria", "orden", "razon", "altar", "volar"
+]
